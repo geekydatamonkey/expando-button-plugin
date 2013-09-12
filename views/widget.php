203aed852d614c25eef1dc0042e982cc19f607c5
James Johnson <johnsojr@gmail.com>
2013-09-12 14:32:08 -0500

    Handle case where not image or text.

--- views/widget.php
+++ views/widget.php
@@ -1,11 +1,29 @@
+<?php
+  // sanitize and cache variables
+  $instance_button_name = sanitize_text_field( $instance['button_name'] );
+  $instance_url = esc_url( $instance['url'] ); 
+  $instance_img_url = esc_url( $instance['img_url'] );
+  $instance_more_text = sanitize_text_field( $instance['more_text']);
+?>
+
 <a href="<?php echo esc_url( $instance['url'] ); ?>" class="expando-button-widget expando-button-widget-<?php echo sanitize_html_class( $instance['button_name'] ); ?>" target="_blank">
-    <div class="expando-button-widget-header"><?php 
-      echo sanitize_text_field( $instance['button_name'] );
-    ?></div>
-    <div class="expando-button-widget-body">
-      <img src="<?php echo esc_url( $instance['img_url'] ); ?>" alt="<?php sanitize_text_field( $instance['button_name'] ); ?>">
-      <div class="expando-button-widget-more-text"><?php 
-      echo sanitize_text_field( $instance['more_text']);
-      ?></div>
-    </div>
+    <div class="expando-button-widget-header"><?php
+      if ($instance_button_name) {
+        echo $instance_button_name;
+      } else {
+        echo "Button";
+      } 
+    ?></div><?php
+    if ($instance_img_url || $instance_more_text ) {
+      ?><div class="expando-button-widget-body">
+        <?php if ($instance_img_url) { ?>
+          <img src="<?php echo $instance_img_url; ?>" alt="<?php echo $instance_button_name; ?>">
+        <?php } 
+        if ($instance_more_text) { ?>
+          <div class="expando-button-widget-more-text"><?php 
+          echo $instance_more_text;
+          ?></div><?php 
+        } ?>
+      </div><?php
+    } // end expando-button-widget-body ?>
 </a>
